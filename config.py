b13c22e2d4b2def7b1e61f336d8736d24b583ab7
David Minge <36607089+mingd0@users.noreply.github.com>
2021-09-12 15:43:46 -0500

    Added filepath constants to config file.

--- config.py
+++ config.py
@@ -2,16 +2,27 @@
 Configuration file to hold user defined parameters
 """
 
-BPWA_FILENAME = '20210716_060000_ST00BP00_dm.csv'
-MEMORY_DATA_FILENAME = 'MC_562_I3_ST00BP00_Digital Data_15JulAM-19JulAM.las'
-EVENTS_FILENAME = 'events.csv'
-TITLE = 'Isabela-3 18-1/8in x 21in Hole Section - RT/MEM Data'
+""" File Paths """
+BPWA_PATH = './data/bpwa/'
+MEMORY_PATH = './data/memory/'
+EVENTS_PATH = './data/events/'
+OUTPUTS_PATH = './outputs/'
+
+# Use None type if file does not exist
+BPWA_FILENAME = '20210910_132929_ST00BP00.csv'
+MEMORY_FILENAME = None
+EVENTS_FILENAME = None
+# No spaces or slashes allowed in output file
+# Recommended format:WellName_DecimalHoleSize_Mem_RT_Data.html
+OUTPUT_FILENAME = 'Isabela-3_12.25inHS_Mem_RT_Data.html'
+
+""" Plotting Preferences """
+TITLE = 'Isabela-3 12-1/4in Hole Section - RT/MEM Data'
 ESD_MARKER_SIZE = 10
 
-# No spaces or slashes permitted in output file.
-# Recommended format:WellName_DecimalHoleSize_Mem_RT_Data.html
-OUTPUT_FILENAME = 'Isabela-3_18.125x21HS_Mem_RT_Data.html'
+""" Mnemonics """
 
+# BPWA
 RT_MNEMONICS = {
   'time': 'TIME',
   'bit_depth': 'GS_DBTM',
@@ -24,6 +35,7 @@ RT_MNEMONICS = {
   'esd_avg': 'ESD'
 }
 
+# Memory (LAS File)
 MEM_MNEMONICS = {
   'time': 'time',
   'ecd': 'ECD'
