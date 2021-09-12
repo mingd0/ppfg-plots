ca0f5a361821bff267742b24a00eb542b3c17b10
David Minge <36607089+mingd0@users.noreply.github.com>
2021-09-12 16:01:05 -0500

    Wrap plotting methods in if-statements so that program doesn't crash in the event that dataframes are None.

--- plot.py
+++ plot.py
@@ -23,93 +23,98 @@ def create_drilling_plot(df_rt, df_mem, df_events):
                             [{"secondary_y": True}],
                             [{"secondary_y": True}]
                         ])
+    if df_rt is not None:
+        # Bit depth
+        fig.add_trace(go.Scatter(x=df_rt.index, y=df_rt[mnem_rt['bit_depth']],
+                                 mode='lines',
+                                 name='Bit Depth'),
+                      row=1, col=1
+                      )
 
-    # Bit depth
-    fig.add_trace(go.Scatter(x=df_rt.index, y=df_rt[mnem_rt['bit_depth']],
-                             mode='lines',
-                             name='Bit Depth'),
-                  row=1, col=1
-                  )
+        # Block position
+        fig.add_trace(go.Scatter(x=df_rt.index,
+                                 y=df_rt[mnem_rt['block_position']],
+                                 mode='lines',
+                                 name='BPOS'),
+                      secondary_y=False,
+                      row=3, col=1
+                      )
 
-    # Block position
-    fig.add_trace(go.Scatter(x=df_rt.index, y=df_rt[mnem_rt['block_position']],
-                             mode='lines',
-                             name='BPOS'),
-                  secondary_y=False,
-                  row=3, col=1
-                  )
+        # Hookload
+        fig.add_trace(go.Scatter(x=df_rt.index, y=df_rt[mnem_rt['hookload']],
+                                 mode='lines',
+                                 name='HKLD'),
+                      secondary_y=True,
+                      row=3, col=1
+                      )
 
-    # Hookload
-    fig.add_trace(go.Scatter(x=df_rt.index, y=df_rt[mnem_rt['hookload']],
-                             mode='lines',
-                             name='HKLD'),
-                  secondary_y=True,
-                  row=3, col=1
-                  )
+        # Standpipe pressure
+        fig.add_trace(go.Scatter(x=df_rt.index, y=df_rt[mnem_rt['spp']],
+                                 mode='lines',
+                                 name='SPPA'),
+                      secondary_y=False,
+                      # yaxis="",
+                      row=2, col=1
+                      )
 
-    # Standpipe pressure
-    fig.add_trace(go.Scatter(x=df_rt.index, y=df_rt[mnem_rt['spp']],
-                             mode='lines',
-                             name='SPPA'),
-                  secondary_y=False,
-                  # yaxis="",
-                  row=2, col=1
-                  )
+        # Realtime ECD
+        fig.add_trace(go.Scatter(x=df_rt.index, y=df_rt[mnem_rt['ecd_rt']],
+                                 mode='lines',
+                                 name='ECD'),
+                      secondary_y=True,
+                      # yaxis="",
+                      row=2, col=1
+                      )
 
-    # Realtime ECD
-    fig.add_trace(go.Scatter(x=df_rt.index, y=df_rt[mnem_rt['ecd_rt']],
-                             mode='lines',
-                             name='ECD'),
-                  secondary_y=True,
-                  # yaxis="",
-                  row=2, col=1
-                  )
+        # ESD_MIN
+        fig.add_trace(go.Scatter(x=df_rt.index, y=df_rt[mnem_rt['esd_min']],
+                                 mode='markers',
+                                 name='ESD_MIN',
+                                 marker_size=ESD_MARKER_SIZE),
+                      secondary_y=True,
+                      # yaxis="",
+                      row=2, col=1
+                      )
 
-    # ESD_MIN
-    fig.add_trace(go.Scatter(x=df_rt.index, y=df_rt[mnem_rt['esd_min']],
-                             mode='markers',
-                             name='ESD_MIN',
-                             marker_size=ESD_MARKER_SIZE),
-                  secondary_y=True,
-                  # yaxis="",
-                  row=2, col=1
-                  )
+        # ESD_MAX
+        fig.add_trace(go.Scatter(x=df_rt.index, y=df_rt[mnem_rt['esd_max']],
+                                 mode='markers',
+                                 name='ESD_MAX',
+                                 marker_size=ESD_MARKER_SIZE),
+                      secondary_y=True,
+                      # yaxis="",
+                      row=2, col=1
+                      )
 
-    # ESD_MAX
-    fig.add_trace(go.Scatter(x=df_rt.index, y=df_rt[mnem_rt['esd_max']],
-                             mode='markers',
-                             name='ESD_MAX',
-                             marker_size=ESD_MARKER_SIZE),
-                  secondary_y=True,
-                  # yaxis="",
-                  row=2, col=1
-                  )
+        # ESD_AVG
+        fig.add_trace(go.Scatter(x=df_rt.index, y=df_rt[mnem_rt['esd_avg']],
+                                 mode='markers',
+                                 name='ESD_AVG',
+                                 marker_size=ESD_MARKER_SIZE),
+                      secondary_y=True,
+                      # yaxis="",
+                      row=2, col=1
+                      )
 
-    # ESD_AVG
-    fig.add_trace(go.Scatter(x=df_rt.index, y=df_rt[mnem_rt['esd_avg']],
-                             mode='markers',
-                             name='ESD_AVG',
-                             marker_size=ESD_MARKER_SIZE),
-                  secondary_y=True,
-                  # yaxis="",
-                  row=2, col=1
-                  )
-
-    # Memory ECD
-    fig.add_trace(go.Scatter(x=df_mem.index, y=df_mem[mnem_mem['ecd']],
-                             mode='lines',
-                             name='Memory ECD'),
-                  secondary_y=True,
-                  # yaxis="",
-                  row=2, col=1
-                  )
+    if df_mem is not None:
+        # Memory ECD
+        fig.add_trace(go.Scatter(x=df_mem.index, y=df_mem[mnem_mem['ecd']],
+                                 mode='lines',
+                                 name='Memory ECD'),
+                      secondary_y=True,
+                      # yaxis="",
+                      row=2, col=1
+                      )
 
     # Add axis titles
     fig.update_yaxes(row=1, col=1, title_text='Bit Depth (ft MD)')
     fig.update_yaxes(row=2, col=1, secondary_y=False,
                      title_text='Pressure (psi)')
-    fig.update_yaxes(row=2, col=1, secondary_y=True, range=[
-                     10.5, 12.5], title_text='ECD/ESD/MW (ppg)')
+    # Use mean ECD to configure axis range
+    ecd_mean = round(df_rt[mnem_rt['ecd_rt']].mean(), 1)
+    fig.update_yaxes(row=2, col=1, secondary_y=True,
+                     range=[(ecd_mean - 1), (ecd_mean + 1)],
+                     title_text='ECD/ESD/MW (ppg)')
     fig.update_yaxes(row=3, col=1, secondary_y=False,
                      title_text='Block Position (ft)')
     fig.update_yaxes(row=3, col=1, secondary_y=True,
@@ -118,13 +123,14 @@ def create_drilling_plot(df_rt, df_mem, df_events):
     # Add graph title
     fig.update_layout(title=TITLE, title_x=0.5)
 
-    # Add annotations
-    df_events['y'] = df_rt.loc[df_events.index, mnem_rt['bit_depth']]
-    annotations = [{
-        'x': df_events.index[i],
-        'y': df_events.iloc[i, 1],
-        'text': df_events.iloc[i, 0],
-    } for i in range(len(df_events.index))]
-    fig.update_layout({"annotations": annotations})
+    if df_events is not None:
+        # Add annotations
+        df_events['y'] = df_rt.loc[df_events.index, mnem_rt['bit_depth']]
+        annotations = [{
+            'x': df_events.index[i],
+            'y': df_events.iloc[i, 1],
+            'text': df_events.iloc[i, 0],
+        } for i in range(len(df_events.index))]
+        fig.update_layout({"annotations": annotations})
 
     return fig
