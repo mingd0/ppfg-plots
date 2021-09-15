749ff60e54f91ffc04681f70e56fd7af7d481afd
Rcc West Ppfg <rccwppfg@bp.com>
2021-09-15 10:21:32 -0500

    Added whp to plot

--- config.py
+++ config.py
@@ -9,9 +9,9 @@ EVENTS_PATH = './data/events/'
 OUTPUTS_PATH = './outputs/'
 
 # Use None type if file does not exist
-BPWA_FILENAME = '20210910_132929_ST00BP00.csv'
+BPWA_FILENAME = '20210908_060000_ST00BP00.csv'
 MEMORY_FILENAME = None
-EVENTS_FILENAME = 'events.csv'
+EVENTS_FILENAME = None
 # No spaces or slashes allowed in output file
 # Recommended format:WellName_DecimalHoleSize_Mem_RT_Data.html
 OUTPUT_FILENAME = 'Isabela-3_12.25inHS_Mem_RT_Data.html'
@@ -28,9 +28,10 @@ RT_MNEMONICS = {
   'bit_depth': 'GS_DBTM',
   'block_position': 'GS_BPOS',
   'rpm': 'GS_RPM',
-  'torque': 'GS_TDTRQ',
+  'torque': 'GS_TQA',
   'hookload': 'GS_HKLDF',
   'spp': 'GS_SPPA',
+  'whp': '9274', 
   'ecd_rt': 'ECD_RT',
   'esd_min': 'ESD_MIN',
   'esd_max': 'ESD_MAX',
